df5970f4c1186319aee99c260194779acb580b76
Jimmy Fan <fanminjian@gmail.com>
2011-05-10 21:36:25 +1000

    add new node future.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,6 +1,25 @@
 class NodesController < ApplicationController
-  
+  before_filter :authorize, :only => [:new]
   def show
     @node = Node.find(params[:id])
+  end
+  
+  def new
+    @node = Node.new
+  end
+  
+  def create
+    @node = Node.new(params[:node])
+
+    respond_to do |format|
+      if @node.save
+        flash[:node] = 'User was successfully created.'
+        format.html { redirect_to(@node) }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 end
