8f96c3ac12ba972f579ee8c6d3de4802788a95ba
Jimmy Fan <fanminjian@gmail.com>
2011-05-13 23:31:50 +1000

    created view of index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @nodes = Node.all
     @user = User.find_by_id(session[:user_id])
+    @users = User.all
     @topics = Topic.all
     @posts = Post.all
   end
