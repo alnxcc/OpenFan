83aed7e64a5e73c7bde5a6d620f52e9909d4c69e
Jimmy Fan <fanminjian@gmail.com>
2011-05-17 02:49:42 +1000

    add switch for user detail set signed in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         flash[:notice] = 'User was successfully created.'
+        session[:user_id] = @user.id
         format.html { redirect_to(@user) }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
