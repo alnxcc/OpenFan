ab07e2410f68b0faa066f89b91a96fb1dccc32ca
Jimmy Fan <fanminjian@gmail.com>
2011-05-18 00:31:28 +1000

    fix order list in home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
     @hotnodes = Node.all(:include => :topics).sort_by { |p| p.topics.count }
     @user = User.find_by_id(session[:user_id])
     @users = User.all
-    @topics = Topic.all(:include => :posts, :order => 'topics.created_at DESC,posts.created_at DESC')
+    #@topics = Topic.all(:include => :posts, :order => 'topics.created_at DESC,posts.created_at DESC')
+    @topics = Topic.all(:include => :posts).sort_by { |p| p.posts.last.nil? ? p.created_at: p.posts.last.created_at}.reverse
     @posts = Post.all
     @title="Home"
   end
