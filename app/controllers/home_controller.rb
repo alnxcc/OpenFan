1ec8cebe34a3be634236e7d57b91d77d6a0a2a48
Jimmy Fan <fanminjian@gmail.com>
2011-05-24 02:26:52 +1000

    fix up count.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
 
     @users = User.all
     #@topics = Topic.all(:include => :posts, :order => 'topics.created_at DESC,posts.created_at DESC')
-    @topics = Topic.all(:include => :posts, :limit => 5).sort_by { |p| p.posts.last.nil? ? p.created_at: p.posts.last.created_at}.reverse
-    @posts = Post.all(:limit=>5).reverse
+    @topics = Topic.all(:include => :posts).sort_by { |p| p.posts.last.nil? ? p.created_at: p.posts.last.created_at}.reverse
+    @posts = Post.all.reverse
     @title="Home"
     respond_to do |format|
       format.html # index.html.erb
