6d9f5020c50ec7abbdc4c597da019c071c80e39a
Jimmy Fan <fanminjian@gmail.com>
2011-05-23 23:15:03 +1000

    updated interface.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,8 @@ class HomeController < ApplicationController
   def index
     @nodes = Node.all
     @hotnodes = Node.all(:include => :topics).sort_by { |p| p.topics.count }
-    if(session[:user_id])
-      @user = User.find_by_id(session[:user_id])
-    else
-      @user = User.new
-    end
+    @user = User.find_by_id(session[:user_id])
+
     @users = User.all
     #@topics = Topic.all(:include => :posts, :order => 'topics.created_at DESC,posts.created_at DESC')
     @topics = Topic.all(:include => :posts).sort_by { |p| p.posts.last.nil? ? p.created_at: p.posts.last.created_at}.reverse
